5c6506780855fee0dc72d62866598d6d94dd38df
待定红尘 <1445493904@qq.com>
2021-02-24 09:00:51 +0800

    完成了商品分类开发

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
+import Cate from '../components/goods/Cate.vue'
 
 const routes = [
   {
@@ -36,7 +37,11 @@ const routes = [
       {
         path: '/roles',
         component: Roles
-      },]
+      },
+      {
+        path: '/categories',
+        component: Cate
+    },]
   },
 ]
 
